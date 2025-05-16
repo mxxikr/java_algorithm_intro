fcb09c01e8ad0d8f06e5df5a4852bde5319547ea
mxxikr <mxxikr@gmail.com>
2025-05-16 11:11:33 +0900

    회문 문자열

---
+++ src/sectionString/palindromeWord.java
@@ -0,0 +1,47 @@
+package sectionString;
+import java.util.Scanner;
+
+/**
+ *설명
+ * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+ * 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+ * 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+ *
+ * 입력
+ * 첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
+ *
+ * 출력
+ * 첫 번째 줄에 회문 문자열인지의 결과를 YES 또는 NO로 출력합니다.
+ *
+ * 예시 입력 1
+ * gooG
+ *
+ * 예시 출력 1
+ * YES
+ */
+public class palindromeWord {
+        public String solution(String str) {
+            String answer = "YES";
+            str = str.toUpperCase();
+
+            int len = str.length(); // 문자열 길이
+
+            // 길이를 절반으로 나눈 몫의 이전 인덱스까지 반복
+            for (int i = 0; i < len / 2; i++) {
+                if (str.charAt(i) != str.charAt(len - i - 1)) {
+                    answer = "NO";
+                    break; // 회문이 아니면 반복문 탈출
+                }
+            }
+
+            return answer;
+        }
+
+        public static void main(String[] args){
+            palindromeWord T = new palindromeWord(); // 객체 생성
+            Scanner kb = new Scanner(System.in); // 콘솔 입출력
+            String str = kb.next(); // 문자열 하나 읽어들임
+
+            System.out.print(T.solution(str)); // 메소드 호출
+        }
+}
